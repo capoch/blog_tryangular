4275475e8fef6dd4ea85876e5f1e40971d50bf8b
capoch <buemplizer@gmail.com>
2017-05-13 22:48:46 +0700

    using ng-resource to connect to mock api up to 21

--- js/app/blog-list/blog-list.component.js
+++ js/app/blog-list/blog-list.component.js
@@ -4,26 +4,10 @@ angular.module('blogList').
   component('blogList', {
     // template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
     templateUrl: '/templates/blog-list.html',
-    controller: function($routeParams, $scope){
-      console.log($routeParams)
-      var blogItems = [
-        {title: "Some title 1", id: 1, description: "This is a book"},
-        {title: "Title 2", id: 2, description: "This is a book"},
-        {title: "This no 3", id: 3, description: "This is a book"},
-        {title: "4 Sho", id: 4, description: "This is a book"}
-      ]
-
-      $scope.items = blogItems;
-
-      $scope.title = 'Hi There faggot'
-      $scope.clicks = 0
-      $scope.someClickTest = function(){
-        console.log("clicked")
-        $scope.clicks += 1
-        $scope.title = "Oh now you clicked it "+$scope.clicks+" times, you miserable cunt"
-      }
+    controller: function(Post, $routeParams, $scope){
+      $scope.items = Post.query();
     }
-  });
+});
   // controller('BlogListController', function($scope){
   //   console.log("hello")
   //   $scope.title = 'Hi There faggot'
